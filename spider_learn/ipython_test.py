0049b5a79b92f2d6f25ccde2ae6403aa81b0e5eb
jiang <304640509@qq.com>
2018-06-07 07:04:50 +0800

    0607 commit money money money

--- spider_learn/ipython_test.py
+++ spider_learn/ipython_test.py
@@ -1,4 +1,5 @@
 import urllib
+<<<<<<< Updated upstream
 word = {"wd": "中文"}
 urllib.urlencode(word)
 "wd=%E4%BC%A0%E6%99%BA%E6%92%AD%E5%AE%A2"
@@ -8,3 +9,6 @@ print(urllib.unquote("wd=%E4%BC%A0%E6%99%BA%E6%92%AD%E5%AE%A2"))
 
 
 
+=======
+word = {"wd":"中文"}·
+>>>>>>> Stashed changes
